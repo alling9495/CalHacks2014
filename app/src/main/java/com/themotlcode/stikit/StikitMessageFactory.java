ecfdcbc6d0974fc3aa58fa284cf2da2895c0f6b3
Alex Ling <alex@home2o.com>
2014-10-04 17:17:14 -0700

    Added message factory class for communication with Receiver app.

---
+++ app/src/main/java/com/themotlcode/stikit/StikitMessageFactory.java
@@ -0,0 +1,50 @@
+package com.themotlcode.stikit;
+
+import org.json.JSONObject;
+import android.util.Log;
+/**
+ * Created by Alex Ling on 10/4/2014.
+ */
+
+public class StikitMessageFactory {
+    private static final String TAG = "StikitMessageFactory";
+    /* Empty constructor */
+    public StikitMessageFactory() {}
+
+    public String Message(String message) {
+        return stikitMessageConstruct(message).toString();
+    }
+
+    public String Commmand(int command) {
+        return stikitCommandConstruct(command).toString();
+    }
+    private JSONObject stikitMessageConstruct(String message) {
+
+        JSONObject messageObj = new JSONObject();
+
+        try {
+            messageObj.put("text", message);
+        }
+        catch (org.json.JSONException e) {
+            Log.d(TAG, e.toString());
+        }
+        return messageObj;
+    }
+    /* Enumerations
+        Left: 0
+        Right: 1
+        Delete: 2
+     */
+    private JSONObject stikitCommandConstruct(int command) {
+        JSONObject commandObj = new JSONObject();
+
+        try {
+            commandObj.put("command", command);
+        }
+        catch (org.json.JSONException e) {
+            Log.d(TAG, e.toString());
+        }
+        return commandObj;
+    }
+
+}
